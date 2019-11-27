be128797b6270ea07942bea9d08a8a787a93e2d2
Christian Richter <christian00richter@gmail.com>
2019-11-27 17:35:46 +0100

    some changes to predict but still not working

---
+++ DogBreedPredictor.py
@@ -0,0 +1,89 @@
+from keras.models import load_model
+from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
+from keras.preprocessing import image
+import pickle
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+
+class DogBreedPredictor_v2():
+    """
+    this class contains every function that is needed for our dog breed prediction
+    """
+
+    def __init__(self):
+        # load list of dog names
+        self.dog_names = pickle.load(open("saved_models/dog_names.p", "rb"))
+        self.model = load_model('saved_models/Resnet50_model.h5')
+
+    def getPredictions(self, img):
+        """
+        checks if the image is predicted to be a dog, a human
+        or something else;
+        if it is a dog it tries to predict its breed
+        """
+        print('getting predictions')
+        predictions = []
+        breed, confidence = self.Resnet50_predict_breed(img)
+        breed = str(breed).split('.')[-1]
+        if self._face_detector(img):
+            predictions.append("You're probably a human!")
+            predictions.append(f"You look like a {breed}")
+        elif self._dog_detector(img):
+            predictions.append(f'Woof! You look like a {breed}. I am {confidence}% sure.')
+        else:
+            predictions.append("I am not sure what kind of species you are.")
+        return predictions
+
+    def Resnet50_predict_breed(self, img_path):
+        """
+        uses our Resnet50 model to make a dog-breed-prediction for an image
+        """
+        # extract bottleneck features
+        bottleneck_feature = self._extract_Resnet50(self._path_to_tensor(img_path))
+        # obtain predicted vector
+        predicted_vector = self.model.predict(bottleneck_feature, batch_size=1, verbose=0)
+        confidence = round(np.max(predicted_vector) * 100, 2)
+        pred_label = self.dog_names[np.argmax(predicted_vector)]
+        # return dog breed that is predicted by the model
+        return pred_label, confidence
+
+    def _path_to_tensor(self, img_path):
+        """
+        converts a given image to a 4D tensor
+        """
+        # loads RGB image as PIL.Image.Image type
+        print(img_path)
+        img = image.load_img(img_path, target_size=(224, 224))
+        # convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
+        x = image.img_to_array(img)
+        # convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
+        return np.expand_dims(x, axis=0)
+
+    def _extract_Resnet50(self, tensor):
+        return ResNet50(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
+
+    def _face_detector(self, img_path):
+        """
+        returns "True" if face is detected in image stored at img_path
+        """
+        img = cv2.imread(img_path)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
+        faces = face_cascade.detectMultiScale(gray)
+        return len(faces) > 0
+
+    def _dog_detector(self, img_path):
+        """
+        returns "True" if a dog is detected in the image stored at img_path
+        """
+        img = preprocess_input(self._path_to_tensor(img_path))
+        prediction = np.argmax(ResNet50(weights='imagenet').predict(img))
+        return ((prediction <= 268) & (prediction >= 151))
+
+
+if __name__ == '__main__':
+    dbp = DogBreedPredictor_v2()
+    preds = dbp.getPredictions("C://work_local//Python//DataScientistND_Project6_Capstone_Project//Dog_Breed//sample_imgs//Bernhardiner.jpg")
+    print(preds)
--- myapp/routes.py
+++ myapp/routes.py
@@ -2,72 +2,97 @@ from myapp import app
 import io
 import os
 import json, plotly
-from flask import render_template, request
-from wrangling_scripts.load_model import init_model
+from flask import Flask, render_template, request, url_for
 from PIL import Image
-from util import decode_prob
-from wrangling_scripts.wrangle_data import DataWrangler
+from DogBreedPredictor import DogBreedPredictor_v2
+import base64
+from PIL import Image
+
+
+
+dbp = DogBreedPredictor_v2()
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', id="pred", preds='Hier kommen die Predictions hin')
+    return render_template('index.html')
+
+# @app.route('/predict', methods=['GET', 'POST'])
+# def predict():
+#     """File selection and display results
+#     """
+#     if request.method == 'POST' and 'file[]' in request.files:
+#         inputs = []
+#         files = request.files.getlist('file[]')
+#         for file_obj in files:
+#             # Check if no files uploaded
+#             if file_obj.filename == '':
+#                 if len(files) == 1:
+#                     return render_template('index.html', preds='Konnte noch keine Aussagen machen')
+#                 continue
+#             entry = {}
+#             entry.update({'filename': file_obj.filename})
+#             try:
+#                 img_bytes = io.BytesIO(file_obj.stream.getvalue())
+#                 entry.update({'data':
+#                               Image.open(
+#                                   img_bytes
+#                               )})
+#             except AttributeError:
+#                 img_bytes = io.BytesIO(file_obj.stream.read())
+#                 entry.update({'data':
+#                               Image.open(
+#                                   img_bytes
+#                               )})
+#             # keep image in base64 for later use
+#             img_b64 = base64.b64encode(img_bytes.getvalue()).decode()
+#             entry.update({'img': img_b64})
+#
+#             inputs.append(entry)
+#
+#         outputs = []
+#
+#         for input_ in inputs:
+#             # perform prediction
+#             out = dbp.getPredictions(input_['data'])
+#             outputs.append(out)
+#
+#
+#         return render_template('predict.html', preds=outputs[0])
+#
+#     # if no files uploaded
+#     return render_template('index.html')
+
+
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     """File selection and display results
     """
-
     if request.method == 'POST' and 'file[]' in request.files:
         inputs = []
+        outputs = []
         files = request.files.getlist('file[]')
-        for file_obj in files:
+        for file in files:
             # Check if no files uploaded
-            if file_obj.filename == '':
+            if file.filename == '':
                 if len(files) == 1:
-                    return render_template('select_files.html')
+                    return render_template('index.html', preds='Konnte noch keine Aussagen machen')
                 continue
             entry = {}
-            entry.update({'filename': file_obj.filename})
-            try:
-                img_bytes = io.BytesIO(file_obj.stream.getvalue())
-                entry.update({'data':
-                              Image.open(
-                                  img_bytes
-                              )})
-            except AttributeError:
-                img_bytes = io.BytesIO(file_obj.stream.read())
-                entry.update({'data':
-                              Image.open(
-                                  img_bytes
-                              )})
-            # keep image in base64 for later use
-            img_b64 = base64.b64encode(img_bytes.getvalue()).decode()
-            entry.update({'img': img_b64})
+            entry.update({'filename': file.filename})
 
-            inputs.append(entry)
+            filename = file.filename
+            file.save(os.path.join("uploads", filename))
+            image_url = url_for('uploaded_file', filename=filename)
 
-        outputs = []
+            out = dbp.getPredictions(image_url)
+            outputs.append(out)
 
-        with graph.as_default():
-            for input_ in inputs:
-                # convert to 4D tensor to feed into our model
-                x = preprocess(input_['data'])
-                # perform prediction
-                out = model.predict(x)
-                outputs.append(out)
 
-        # decode output prob
-        outputs = decode_prob(outputs)
 
-        results = []
 
-        for input_, probs in zip(inputs, outputs):
-            results.append({'filename': input_['filename'],
-                            'image': input_['img'],
-                            'predict_probs': probs})
-
-        return render_template('results.html', results=results)
+        return render_template('predict.html', preds=outputs[0])
 
     # if no files uploaded
-    return render_template('select_files.html')
+    return render_template('index.html')
