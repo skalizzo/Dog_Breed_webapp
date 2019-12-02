47cf7ec01dcf9d93a8f78050ab7744e7e18bc901
Christian Richter <christian00richter@gmail.com>
2019-12-02 16:09:26 +0100

    feat: included loader animation and error messages within index.html

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,14 +1,13 @@
 import os
-from flask import Flask, flash, request, redirect, url_for
+from flask import Flask, flash, request, redirect, url_for, Blueprint
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = '.\\uploads'
+#UPLOAD_FOLDER = '/static/uploads'
 
-app = Flask(__name__,
-            static_url_path='',
-            static_folder='myapp/static',
-            template_folder='myapp/templates',
-            )
+app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'super_secret_key'
+app.config['SESSION_TYPE'] = 'filesystem'
 
 from myapp import routes
--- myapp/routes.py
+++ myapp/routes.py
@@ -1,16 +1,10 @@
 from werkzeug.utils import secure_filename
 from myapp import app
-import io
 import os
-import json, plotly
 from flask import Flask, render_template, request, url_for, flash, redirect, send_from_directory, Markup
-from PIL import Image
 from DogBreedPredictor import DogBreedPredictor_v2
-import base64
-from PIL import Image
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
-
 dbp = DogBreedPredictor_v2()
 
 @app.route('/')
@@ -21,9 +15,19 @@ def index():
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+    print(f'uploaded file gestartet für {filename}')
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
+
+@app.route('/static/uploads/<filename>')
+def show_file2(filename):
+    print('show file gestartet')
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/show/<filename>')
+def show_file(filename):
+    print('showing file')
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -33,27 +37,24 @@ def allowed_file(filename):
 def predict():
     """File selection and display results
     """
+    error_message = ""
     if request.method == 'POST':
         # double checking if a file has been uploaded
         if 'file[]' not in request.files:
-            return render_template('index.html', preds="Error predicting breed. file not found.")
-        file = request.files['file[]']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            prediction = dbp.getPredictions("." + url_for('uploaded_file', filename=filename))
-            print(prediction)
-            display_image = url_for('uploaded_file', filename=filename)
-            print(display_image)
-            full_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            print(full_filename)
-
-            display_image = Markup(f'<img src="{display_image}" alt="Image used for prediction" height="250">')
-
-            return render_template('show_prediction.html', prediction=" ".join(prediction), predicted_image=display_image)
-    return render_template('index.html')
+            error_message="Error predicting breed. file not found."
+        else:
+            file = request.files['file[]']
+            # if user does not select file, browser also
+            # submit an empty part without filename
+            if file.filename == '':
+                error_message = 'No selected file'
+            else:
+                if file and allowed_file(file.filename):
+                    filename = secure_filename(file.filename)
+                    print(f"saving to {os.path.join(app.config['UPLOAD_FOLDER'], filename)}")
+                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                    image_path = url_for('uploaded_file', filename=filename)
+                    prediction = dbp.getPredictions("." + image_path)
+                    #display_image = Markup(f"""<img src="{app.root_path + '/show/' + filename}" alt="Image used for prediction" height="250">""")
+                    return render_template('show_prediction.html', prediction=" ".join(prediction))
+    return render_template('index.html', error_message=error_message)
